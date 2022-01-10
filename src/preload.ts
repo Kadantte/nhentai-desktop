f21b2a65fc252e641f6cf5095d4556481a648802
Chattipoom <55627201+LittleBlue512@users.noreply.github.com>
2022-01-10 17:51:21 +0700

    Clean up

--- src/preload.ts
+++ src/preload.ts
@@ -13,45 +13,40 @@ window.addEventListener('DOMContentLoaded', () => {
   downloadBtn.addEventListener('click', onDownload);
 
   async function onDownload() {
+    // Prepare the data.
     const url = (<HTMLInputElement>itemUrlEl).value;
     if (url === '') throw new Error('URL is invalid.');
-    try {
-      const itemDoc = await getItemDoc(url);
-      const itemData = await getItemData(itemDoc);
-      const imgExt = getImageExtension(itemData);
-      if (imgExt === null) throw new Error('Cannot find the image extension.');
-      await ipcRenderer.invoke('download', {
-        payload: { itemData, galleryUrl, imgExt },
-      });
-    } catch (err) {
-      console.error(err);
-    }
+    const itemDoc = await getItemDoc(url);
+    const itemData = await getItemData(itemDoc);
+    const imgExt = getImageExtension(itemData);
+    if (imgExt === null) throw new Error('Cannot find the image extension.');
+
+    // Invoke the download function in the main process.
+    await ipcRenderer.invoke('download', {
+      payload: { itemData, galleryUrl, imgExt },
+    });
   }
 
   async function onFetch(e: SubmitEvent): Promise<void> {
     e.preventDefault();
+
+    // Prepare the data.
     const url = (<HTMLInputElement>itemUrlEl).value;
-    if (url === '') {
-      // TODO: Notify the user
-      throw new Error('URL is invalid.');
-    }
-    try {
-      const itemDoc = await getItemDoc(url);
-      itemContentEl.innerHTML = '';
-      const itemData = await getItemData(itemDoc);
-      const imgExt = getImageExtension(itemData);
-      if (imgExt === null) throw new Error('Cannot find image extension.');
-      const itemTitle = itemData.title.english;
-      itemTitleEl.innerHTML = itemTitle;
-      for (var i = 1; i <= itemData.num_pages; i++) {
-        const imgEl = document.createElement('img');
-        imgEl.src = `${galleryUrl}/${itemData.media_id}/${i}${imgExt}`;
-        imgEl.alt = `Image ${i}`;
-        itemContentEl.appendChild(imgEl);
-      }
-    } catch (err) {
-      // TODO: Notify the user
-      console.error(err);
+    if (url === '') throw new Error('URL is invalid.');
+    const itemDoc = await getItemDoc(url);
+    itemContentEl.innerHTML = '';
+    const itemData = await getItemData(itemDoc);
+    const imgExt = getImageExtension(itemData);
+    if (imgExt === null) throw new Error('Cannot find image extension.');
+    const itemTitle = itemData.title.english;
+    itemTitleEl.innerHTML = itemTitle;
+
+    // Fetch the images.
+    for (var i = 1; i <= itemData.num_pages; i++) {
+      const imgEl = document.createElement('img');
+      imgEl.src = `${galleryUrl}/${itemData.media_id}/${i}${imgExt}`;
+      imgEl.alt = `Image ${i}`;
+      itemContentEl.appendChild(imgEl);
     }
   }
 });
@@ -73,11 +68,7 @@ async function getItemData(itemDoc: Document) {
 
 function getImageExtension(itemData: any): string | null {
   const type = itemData.images.cover.t;
-  if (type === 'j') {
-    return '.jpg';
-  } else if (type === 'p') {
-    return '.png';
-  } else {
-    return null;
-  }
+  if (type === 'j') return '.jpg';
+  else if (type === 'p') return '.png';
+  else return null;
 }
