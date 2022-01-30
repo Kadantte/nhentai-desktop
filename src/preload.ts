2afc568de1b95240adbd7f7671556bb5cf736358
Chattipoom <55627201+LittleBlue512@users.noreply.github.com>
2022-01-30 18:34:54 +0700

    Update download button

--- src/preload.ts
+++ src/preload.ts
@@ -3,38 +3,14 @@ import { ipcRenderer } from 'electron';
 const { BASE_API_URL, BASE_IMG_URL } = require('../src/config.json');
 
 window.addEventListener('DOMContentLoaded', () => {
+  const btnSlotEl = document.getElementById('btn-slot');
   const bookUrlEl = document.getElementById('book-url');
   const fetchBtnEl = document.getElementById('fetch-btn');
   const bookTitleEl = document.getElementById('book-title');
-  const downloadBtn = document.getElementById('download-btn');
   const bookContentEl = document.getElementById('book-content');
   const bookPageCntEl = document.getElementById('book-page-cnt');
 
   fetchBtnEl.addEventListener('click', onFetch);
-  downloadBtn.addEventListener('click', onDownload);
-
-  async function onDownload() {
-    // Grab the url.
-    const url = (<HTMLInputElement>bookUrlEl).value;
-    if (url === '') throw new Error('URL is invalid.');
-
-    // Get the book id.
-    const bookId = url.match(/[0-9]/g).join('');
-    if (bookId.length !== 6) throw new Error('Book ID is invalid.');
-
-    // Fetch the book.
-    const response = await fetch(`${BASE_API_URL}/${bookId}`);
-    const book = await response.json();
-
-    // Get the image extension.
-    const imgExt = getImageExtension(book);
-    if (imgExt === null) throw new Error('Cannot find image extension.');
-
-    // Invoke the download function in the main process.
-    await ipcRenderer.invoke('download', {
-      payload: { book, imgExt },
-    });
-  }
 
   async function onFetch(e: SubmitEvent): Promise<void> {
     e.preventDefault();
@@ -55,13 +31,10 @@ window.addEventListener('DOMContentLoaded', () => {
     const imgExt = getImageExtension(book);
     if (imgExt === null) throw new Error('Cannot find image extension.');
 
-    // Replace the title.
+    // Insert book info.
     bookTitleEl.innerHTML = book.title.english;
-
-    // Replace the page count.
     bookPageCntEl.innerHTML = book.num_pages + ' Pages';
-
-    // Clear the content.
+    btnSlotEl.appendChild(buildDownloadBtn(url));
     bookContentEl.innerHTML = '';
 
     // Insert the images.
@@ -84,4 +57,35 @@ function getImageExtension(book: any): string | null {
     default:
       return null;
   }
+}
+
+async function onDownload(url: string) {
+  // Validate the url.
+  if (url === '') throw new Error('URL is invalid.');
+
+  // Get the book id.
+  const bookId = url.match(/[0-9]/g).join('');
+  if (bookId.length !== 6) throw new Error('Book ID is invalid.');
+
+  // Fetch the book.
+  const response = await fetch(`${BASE_API_URL}/${bookId}`);
+  const book = await response.json();
+
+  // Get the image extension.
+  const imgExt = getImageExtension(book);
+  if (imgExt === null) throw new Error('Cannot find image extension.');
+
+  // Invoke the download function in the main process.
+  await ipcRenderer.invoke('download', {
+    payload: { book, imgExt },
+  });
+}
+
+function buildDownloadBtn(downloadUrl: string) {
+  const downloadBtnEl = document.createElement('span');
+  downloadBtnEl.id = 'download-btn';
+  downloadBtnEl.className = 'material-icons icon-button';
+  downloadBtnEl.innerText = 'download';
+  downloadBtnEl.addEventListener('click', () => onDownload(downloadUrl));
+  return downloadBtnEl;
 }
